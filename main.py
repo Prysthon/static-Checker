f1ce23467e749ab45a1eb8437186a360cd5500cf
Hugo Mamona <hugoandrademm@gmail.com>
2024-06-09 21:58:30 -0300

    Main adaptada pras mudanças no lexer

--- main.py
+++ main.py
@@ -2,38 +2,42 @@
 
 from lexer import Lexer
 from symbol_table import SymbolTable
-from word_table import WordTable
 from relatorio import Relatorio
 from utils import read_file, write_file, determinar_escopo
 from header import header
+
 
 def main():
     # 1º: abrir o arquivo e salvar o endereço
 
     # file = input('Digite o nome do arquivo: ')
-    file = 'meuTeste'
+    file = "meuTeste"
     source_code = read_file(file)
 
     # 2º: inicializar variaveis e objetos
     symbol_table = SymbolTable()
-    word_table = WordTable()
     lexer = Lexer(source_code)
-    
-    position = 0
-    tokens_dados = []
-    escopo = ''
-    previous_token = ''
+
+    inicio_lex = 0
+    tokens_dados_p_relatorio = []
+    escopo = ""
+    previous_token = ""
 
     # 3º: loop analisador lexico
-    while position < len(source_code):
+    while inicio_lex < len(source_code):
+
         escopo = determinar_escopo(previous_token)
-        previous_token, position = lexer.tokenize(position, escopo, symbol_table, word_table)
         
-    tokens_dados = lexer.get_tokens_dados()
+        previous_token, inicio_lex = lexer.formar_atomo(        
+            inicio_lex, escopo, symbol_table
+        )
+
+    tokens_dados_p_relatorio = lexer.get_tokens_dados()
 
     # 4º: gerar relatorios .LEX e .TAB
-    relatorio = Relatorio(tokens_dados)
+    relatorio = Relatorio(tokens_dados_p_relatorio)
     relatorio.gerar_lex(file)
+
 
 #     symbol_table = SymbolTable()
 
