07a8911f58762bc2ec9d772329ac44bdd3aeda24
Hugo Mamona <hugoandrademm@gmail.com>
2024-06-13 20:14:31 -0300

    Adcionado função para pegar o tipo de átomo Adcionado função pra usar regex pra deixar o texto todo em maiúsculo exceto sequências de escape (\n,\t,\r)

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 # utils.py
 
+import re
+from palavras import PALAVRAS
+
 def read_file(file_path):
     with open(f'{file_path}.241', 'r') as file:
         return file.read()
@@ -7,6 +10,16 @@ def read_file(file_path):
 def write_file(file_path, content):
     with open(file_path, 'w') as file:
         file.write(content)
+
+def uppercase_menos_sequencias(string):
+    # Function to convert match to uppercase if it's not an escape sequence
+    def to_upper_except_escapes(match):
+        if match.group() in ['\\n', '\\t', '\\r']:
+            return match.group()
+        return match.group().upper()
+
+    # Replace each match using the custom function
+    return re.sub(r'\\[ntr]|.', to_upper_except_escapes, string)
 
 def determinar_escopo(previous_token):
     # : -> nomFuncao
@@ -20,3 +33,9 @@ def determinar_escopo(previous_token):
     # variavel
     else:
         return 'C07'
+    
+def get_codigo(lexeme):
+    for padrao, codigo in PALAVRAS:
+        if re.fullmatch(padrao, lexeme):
+            return codigo
+    return None
